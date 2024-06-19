43bcddc6df95f4c520c14bea5f9a70ba663330bd
asyncdoggo <ayushdeshpande81@gmail.com>
2024-06-19 17:55:43 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+
+def setup_app():
+
+    FIREBASE_CREDENTIALS = {
+        "type": "service_account",
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+        "client_email": os.getenv("CLIENT_EMAIL"),
+        "client_id": os.getenv("CLIENT_ID"),
+        "auth_uri": os.getenv("AUTH_URI"),
+        "token_uri": os.getenv("TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+        "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+    }
+
+    import json
+
+    with open("firebase-credentials.json", "w") as f:
+        json.dump(FIREBASE_CREDENTIALS, f)
