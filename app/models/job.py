asyncdoggo/job-hunter

commit 6e36ff33e3e463dbb58d791e026f292b62ad236d

Added new fields in JobBoard model

app/models/job.py
@@ -14,13 +14,22 @@ class JobStatus(str, Enum):
     REJECTED = "rejected"
     FOLLOW_UP = "follow up"
 
+class JobType(str, Enum):
+    Onsite = "onsite"
+    Hybrid = "hybrid"
+    REMOTE = "remote"
+
 
 class JobBoard(BaseModel):
-    status: JobStatus
     company_name: str
     position: str
+    status: JobStatus
     salary: str
+    job_type: str
     location: str
     job_url: str
+    applied_date: str
+    deadline: str
+    description: str
     id: str  = None
     user_id: Optional[str] = None

app/routers/job_board.py
@@ -6,8 +6,6 @@ from app.models.job import JobBoard
 from app.db.firebasedb import FireBaseDatabase
 
 
-
-
 router = APIRouter()
 
 
@@ -17,6 +15,12 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# Job Board routes
+@router.get("/job")
+async def test_job():
+    print("Job Board routes")
+    return {"message": "Job Board routes"}
 
 # Job Board query
 @router.get("/job/board")

commit 22bb9c9b2f778c5f4e1326766bb424eebc9ea074

Fix errors introduced by 6e36ff33e3e463dbb58d791e026f292b62ad236d

app/models/job.py
@@ -1,4 +1,3 @@
-
 from enum import Enum
 from typing import Optional
 
@@ -15,9 +14,10 @@ class JobStatus(str, Enum):
     FOLLOW_UP = "follow up"
 
 class JobType(str, Enum):
-    Onsite = "onsite"
-    Hybrid = "hybrid"
+    ONSITE = "onsite"
+    HYBRID = "hybrid"
     REMOTE = "remote"
+    NOT_SPECIFIED = "not specified"
 
 
 class JobBoard(BaseModel):
@@ -25,11 +25,10 @@ class JobBoard(BaseModel):
     position: str
     status: JobStatus
     salary: str
-    job_type: str
+    job_type: JobType
     location: str
     job_url: str
     applied_date: str
     deadline: str
-    description: str
-    id: str  = None
+    id: Optional[str] = None
     user_id: Optional[str] = None

app/routers/job_board.py
@@ -1,3 +1,4 @@
+import random
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
@@ -16,11 +17,6 @@ def get_db():
     finally:
         db.close()
 
-# Job Board routes
-@router.get("/job")
-async def test_job():
-    print("Job Board routes")
-    return {"message": "Job Board routes"}
 
 # Job Board query
 @router.get("/job/board")
@@ -33,7 +29,7 @@ async def job_board_get(token: str = Depends(get_current_user), db: FireBaseData
         jobs = db.query({"user_id": user_id})
         return {"jobs": jobs}
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
 
 
 # Job Board insert
@@ -44,10 +40,14 @@ async def job_board_insert(job: JobBoard, token: str = Depends(get_current_user)
 
     job_data = job.model_dump()
     job_data["user_id"] = token["user_id"]
+
+    if not job_data["id"]:
+        job_data["id"] = str(random.randint(1000000000, 9999999999))
+
     try:
         db.insert(job_data)
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
     return {"message": "Job added successfully", "job": job_data}
 
 # Job Board update
@@ -62,7 +62,7 @@ async def job_board_update(job: JobBoard, token: str = Depends(get_current_user)
         db.update(job_data)
         return {"message": "Job updated successfully", "job": job_data}
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
 
 
 # Job Board delete
@@ -75,5 +75,5 @@ async def job_board_delete(job_id: str, token: str = Depends(get_current_user), 
     try:
         db.delete({"user_id": user_id, "id": job_id})
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
     return {"message": "Job deleted successfully"}

app/routers/job_search.py
@@ -10,14 +10,13 @@ from ..job_utils.resume import ResumeParser
 from ..job_utils.scraper import linkedinJobSpyScraper
 from typing import List
 import json
-import typing
 
 
 router = APIRouter()
 
 from pydantic import BaseModel
 
-class JobMatcherRequest(BaseModel):
+class JobSearchRequest(BaseModel):
     search_term: str
     location: str
 
@@ -53,8 +52,22 @@ async def job_matching(search_term: str=Form(...), location: str=Form(...), resu
     return JSONResponse(content={"matched_jobs": matched_jobs}, status_code=status.HTTP_200_OK)
     
     
+
+class JobResponseModel(BaseModel):
+    id: str
+    job_url: str 
+    title: str # position
+    company: str # company_name
+    location: str
+    job_type: str
+
+
+class JobSearchResponse(BaseModel):
+    jobs: List[JobResponseModel]
+
+
 @router.post("/job/search")
-async def job_search(search_data: JobMatcherRequest, token: str = Depends(get_current_user)):
+async def job_search(search_data: JobSearchRequest, token: str = Depends(get_current_user)):
     if "error" in token:
         raise HTTPException(status_code=400, detail=token["error"])
 
@@ -69,6 +82,8 @@ async def job_search(search_data: JobMatcherRequest, token: str = Depends(get_cu
         jobs = json.dumps(jobs, default=str)
         jobs = json.loads(jobs)
 
+        jobs = [{key: value for key, value in job.items() if key in JobResponseModel.model_fields} for job in jobs]
+
         return JSONResponse(content={"jobs": jobs}, status_code=status.HTTP_200_OK)
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e