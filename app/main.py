6248c477eacfdcae3f9373d9222a756ea8a43c31
asyncdoggo <ayushdeshpande81@gmail.com>
2024-07-24 12:47:29 +0530

    Update URLs for CORS

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["jobhunt-ea01a.web.app","jobhunt-ea01a.firebaseapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
